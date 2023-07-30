91f084b2b1f53521ab44bc06327ea105d72dfd53
gleb226 <gmedvede226@gmail.com>
2023-07-30 19:40:32 +0300

    array 6

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,17 @@ fn main() {
     // array
 
 
-    let array = [1, 2, 3, 4, 5, 6, 7, 8];
+    let array = [1, 2, 2, 4, 5, 6, 7, 7, 8, 9, 9];
 
-    for i in array.iter() {
-        println!("{}", i);
+    let mut i = 0;
+    while i < array.len() {
+        let mut j = i + 1;
+        while j < array.len() {
+            if array[i] == array[j] {
+                println!("{}", array[i]);
+            }
+            j += 1;
+        }
+        i += 1;
     }
-
 }
