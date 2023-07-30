152679110d010057e4584fa9c0bd2c9143eb3f60
gleb226 <gmedvede226@gmail.com>
2023-07-30 19:21:42 +0300

    array 3

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 fn main() {
     // array
 
-    let array = [1, 2, 3, 4, 5];
-    //                   0  1  2  3  4
-    println!("{:?}", array[3]);
+
+    let mut array: [i8; 5] = [1, 2, 3, 4, 5];
+    //                         0  1  2  3  4
+
+    array[2] = 10;
+    array[0] = 2;
+    array[1] = 13;
+    array[3] = 43;
+    array[4] = 1;
+
+    println!("{}", array[2]);
+    println!("{:?}", array);
 }
